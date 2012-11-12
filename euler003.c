8f096ac81c7b0b97747efad51d463b0a96d521ea
Edwin Smulders <mail@dutchy.org>
2012-11-12 01:03:45 +0100

    Implement 'factor' functionality.

--- euler003.c
+++ euler003.c
@@ -1,25 +1,53 @@
 #include <stdio.h>
 #include <math.h>
 #include "euler003.h"
+#include <stdlib.h>
 
 int main( int argc, char *argv[])
 {
-	printf("The prime factors of 13195 are 5, 7, 13 and 29.\n");
-	printf("What is the largest prime factor of the number 600851475143 ?\n");
+	if(argc == 1)
+	{
+		printf("The prime factors of 13195 are 5, 7, 13 and 29.\n");
+		printf("What is the largest prime factor of the number 600851475143 ?\n");
+		int primes[100];
+		int p = 0;
+		prime_factor(600851475143L, &p, primes);
+		int max = 0;
+		for ( int i = 0; i < p; i++)
+			if(primes[i] > max) max = primes[i];
+		printf("Answer: %d\n", max);
+	}
+	else 
+	{
+		unsigned long factor = atol(argv[1]);
+		int primes[100];
+		int p = 0;
+		prime_factor(factor, &p, primes);
+		printf("%lu:", factor);
+		bubblesort(primes, p);
+		for( int i = 0; i < p; i++)
+			printf(" %d", primes[i]);
+		printf("\n");
+	}
+}
 
-	//printf("Answer factor 600851475143: %lu\n", factor(600851475143)[0]);
-	int primes[100];
-	int p = 0;
-//	prime_factor(105, &p, primes);
-//	p = 0;
-//	prime_factor(44, &p, primes);
-//	p = 0;
-//	prime_factor(1234509, &p, primes);
-	prime_factor(600851475143L, &p, primes);
-	int max = 0;
-	for ( int i = 0; i < p; i++)
-		if(primes[i] > max) max = primes[i];
-	printf("Answer: %d\n", max);
+void bubblesort(int *array, int p)
+{
+	int sane = 0;
+	while(!sane) 
+	{
+		sane = 1;
+		for( int i = 0; i < p-1; i++)
+		{
+			if (array[i+1] < array[i]) 
+			{
+				int tmp = array[i];
+				array[i] = array[i+1];
+				array[i+1] = tmp;
+				sane = 0;
+ 			}
+		}
+	}
 }
 
 void prime_factor(long n, int *p, int *primes)
--- euler003.h
+++ euler003.h
@@ -2,3 +2,4 @@
 void prime_factor(long n, int *p, int *primes);
 void factor(long n, long *factor1, long *factor2);
 int is_square(long x);
+void bubblesort(int *array, int p);
